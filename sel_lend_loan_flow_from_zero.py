c8f8524342afb23277c4670320b8f5a3c2f9ee2a
@erfan.shafiee <erfan.shafiee@dgshahr.com>
2025-08-20 15:07:13 +0330

    feat : add url validation

--- sel_lend_loan_flow_from_zero.py
+++ sel_lend_loan_flow_from_zero.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 
 from constants import NATIONAL_CODE, MAX_VALUE, POSTAL_CODE, URL, PHONE_NUMBER
 from functions import *
@@ -21,6 +22,14 @@ class LoanAutomation:
         options.add_argument('--log-level=3')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+
+    def check_current_url(self, expected_url, message):
+        """Function to check if the current URL matches the expected URL and print a message."""
+        current_url = self.driver.current_url
+        print(current_url)
+        if current_url.startswith(expected_url):
+            print(f"###################### {message} = pass ###################")
 
     def run(self):
         step_methods = {
@@ -41,14 +50,21 @@ class LoanAutomation:
 
     def _login_and_navigate(self):
         get_url(self.driver, URL)
+        self.check_current_url(URL , "get url")
         sleep(5)
         login(self.driver, PHONE_NUMBER)
         sleep(5)
         otp_code(self.driver)
         sleep(5)
+        self.check_current_url("https://alpha.dgstack.ir/lend/" , "login")
         loan_request(self.driver, "Down")
         sleep(3)
-        self.driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[2]/div/a/button').click()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" , "loan_request_down")
+        try:
+            self.driver.find_element(By.XPATH, '/html/body/div/div[2]/div/div[2]/div/a/button').click()
+            print("###################### countiue_button = pass ###################")
+        except (NoSuchElementException, ElementClickInterceptedException) as e:
+            print("###################### countiue_button = fail ###################", e)
         sleep(3)
 
     def _select_guarantee_and_next(self):
@@ -64,6 +80,7 @@ class LoanAutomation:
         sleep(2)
         next_button(self.driver)
         sleep(3)
+        
 
     def _upload_identity_residence_job_docs(self):
         Upload_identity_documents(self.driver, self.file_path_low_size)
@@ -71,6 +88,7 @@ class LoanAutomation:
         next_button(self.driver)
 
         sleep(3)
+        self.check_current_url("https://alpha.dgstack.ir/lend/residence-document/" , "identity_documents")
         Residence_documents(
             self.driver, POSTAL_CODE, "'مالک'", "'تهران'", "'تهران'",
             "'آدرس تست'", self.file_path_low_size, "'آدرس تست'"
@@ -79,6 +97,7 @@ class LoanAutomation:
         next_button(self.driver)
 
         sleep(5)
+        self.check_current_url("https://alpha.dgstack.ir/lend/employment-document/" , "Residence_documents")
         Upload_job_documents(self.driver, "'۱۵ تا ۲۰ میلیون تومان'", self.file_path_low_size)
         sleep(3)
         next_button(self.driver)
@@ -86,83 +105,122 @@ class LoanAutomation:
 
     def _step_fresh_start(self):
         get_url(self.driver, URL)
+        self.check_current_url(URL , "get url")
         sleep(5)
         login(self.driver, PHONE_NUMBER)
         sleep(5)
         otp_code(self.driver)
         sleep(5)
+        self.check_current_url("https://alpha.dgstack.ir/lend/" , "login")
         loan_request(self.driver, "TOP")
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/personal-info/" , "loan_request_top")
         self.driver.back()
         sleep(2)
         loan_request(self.driver, "Down")
-
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/personal-info/" , "loan_request_down")
         primary_info(self.driver, NATIONAL_CODE, "1383", "آبان", "24", "'کارمند رسمی (شرکت دولتی)'", "'سایر'")
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/credit-rank/" , "primary_info")
         sleep(10)
-        self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/button').click()
-
+        try:
+            self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/button').click()
+            print("###################### cross_button = pass ###################")
+        except (NoSuchElementException, ElementClickInterceptedException) as e:
+            print("###################### cross_button = fail ###################", e)
+        
         while not is_credit_approved(self.driver):
             sleep(5)
+            print("###################### is_credit_approved = RUNNING ###################")
         sleep(3)
+        if is_credit_approved(self.driver) == True:
+            print("###################### is_credit_approved = pass ###################")
         next_button(self.driver)
+        #todo : if next button error
         sleep(3)
-
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-request/" , "credit_rank_page")
         self._select_guarantee_and_next()
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/identity-document/" , "gurantee_page")
         self._upload_identity_residence_job_docs()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" , "Upload_job_documents")
+
 
     def _step_auth_otp(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/credit-rank/" , "login_and_navigate_to_credit_rank")
         sleep(10)
-        self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/button').click()
+        try:
+            self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/button').click()
+            print("###################### cross_button = pass ###################")
+        except (NoSuchElementException, ElementClickInterceptedException) as e:
+            print("###################### cross_button = fail ###################", e)
 
         while not is_credit_approved(self.driver):
             sleep(5)
+            print("###################### is_credit_approved = RUNNING ###################")
         sleep(3)
+        if is_credit_approved(self.driver) == True:
+            print("###################### is_credit_approved = pass ###################")
         next_button(self.driver)
         sleep(3)
-
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-request/" , "credit_rank_page" )
         self._select_guarantee_and_next()
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/identity-document/" ,"gurantee_page" )
         self._upload_identity_residence_job_docs()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" ,"Upload_job_documents" )
 
     def _step_credit_rank(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-request/" ,"_login_and_navigate_to_loan-request" )
         self._select_guarantee_and_next()
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/identity-document/" ,"gurantee_page")
         self._upload_identity_residence_job_docs()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" , "Upload_job_documents")
 
     def _step_loan_request(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/user/identity-document/" ,"_login_and_navigate_to_identity_page")
         Upload_identity_documents(self.driver, self.file_path_low_size)
         sleep(5)
         next_button(self.driver)
         sleep(3)
+        self.check_current_url("https://alpha.dgstack.ir/lend/residence-document/" , "identity_documents")
         Residence_documents(self.driver, POSTAL_CODE, "'مالک'", "'تهران'", "'تهران'", "'آدرس تست'", self.file_path_low_size, "'آدرس تست'")
         sleep(3)
         next_button(self.driver)
         sleep(5)
+        self.check_current_url("https://alpha.dgstack.ir/lend/employment-document/" , "Residence_documents")
         Upload_job_documents(self.driver, "'۱۵ تا ۲۰ میلیون تومان'", self.file_path_low_size)
         sleep(3)
         next_button(self.driver)
         sleep(10)
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/","Upload_job_documents")
 
     def _step_identity(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/residence-document/" , "login_and_navigate_to_residence-document")
         Residence_documents(self.driver, POSTAL_CODE, "'مالک'", "'تهران'", "'تهران'", "'آدرس تست'", self.file_path_low_size, "'آدرس تست'")
         sleep(3)
         next_button(self.driver)
         sleep(5)
+        self.check_current_url("https://alpha.dgstack.ir/lend/employment-document/" , "Residence_documents" )
         Upload_job_documents(self.driver, "'۱۵ تا ۲۰ میلیون تومان'", self.file_path_low_size)
         sleep(3)
         next_button(self.driver)
         sleep(10)
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" ,"Upload_job_documents")
 
     def _step_residence(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/employment-document/" , "login_and_navigate_to_employment-document")
         Upload_job_documents(self.driver, "'۱۵ تا ۲۰ میلیون تومان'", self.file_path_low_size)
         sleep(3)
         next_button(self.driver)
         sleep(10)
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" , "Upload_job_documents")
+
 
     def _step_branch(self):
         self._login_and_navigate()
+        self.check_current_url("https://alpha.dgstack.ir/lend/loan-info/" , "loan_info_last_page")
         sleep(10)
 
 
