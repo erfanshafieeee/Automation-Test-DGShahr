2a17209d902dd11429bfc8ca4e85f02f7a19edd0
@erfan.shafiee <erfan.shafiee@dgshahr.com>
2025-08-25 17:35:44 +0330

    feat : add new function for update testcase status manualy

--- TCMS_tools/tcms_fuctions.py
+++ TCMS_tools/tcms_fuctions.py
@@ -20,3 +20,16 @@ def set_exec_status(rpc , runner_id ,case_summary: str, ok: bool):
     for e in executions:
         rpc.TestExecution.update(e["id"], {"status": status_id})
         print(f"[TCMS] {case_summary} -> Execution {e['id']} set to {status_key} ({status_id})")
+
+def set_exec_status_manualy(rpc , runner_id ,case_summary: str, status: str):
+    """Update manualy execution status for a case in the active run."""
+    case_id = _get_case_id_in_run(rpc , runner_id, case_summary)
+    executions = rpc.TestExecution.filter({"run_id": runner_id, "case": case_id})
+    if not executions:
+        raise RuntimeError(f"No executions for case_id={case_id} in run_id={runner_id}")
+
+    status_id = tcms_maps.EXECUTION_STATUSES[status]
+
+    for e in executions:
+        rpc.TestExecution.update(e["id"], {"status": status_id})
+        print(f"[TCMS] {case_summary} -> Execution {e['id']} set to {status} ({status_id})")
